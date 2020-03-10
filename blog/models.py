52e25a7e7cb6aa3eaf13c3983552214f34a9d3d5
chankokchen <kokchen2014@outlook.com>
2020-03-10 12:21:00 +0800

    Update template design

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,37 @@
 from django.db import models
 
 class Blog(models.Model):
+    GENERAL = 'General'
+    WORK = 'Work'
+    BUSINESS = 'Business'
+    FOOD = 'Food'
+    MOVIES = 'Movies'
+    BOOKS = 'Books'
+    MOM = 'Mom'
+    TRAVEL = 'Travel'
+    LIFESTYLE = 'Lifestyle'
+    DIY = 'DIY'
+    PARENTING = 'Parenting'
+    SOCIAL = 'Social'
+    STUDY= 'Study'
+    DAILY= 'Daily'
+    CATEGORY_CHOICES = [
+        (GENERAL, 'General'),
+        (WORK, 'Work'),
+        (BUSINESS, 'Business'),
+        (FOOD, 'Food'),
+        (MOVIES, 'Movies'),
+        (BOOKS, 'Books'),
+        (MOM, 'Mom'),
+        (TRAVEL, 'Travel'),
+        (LIFESTYLE, 'Lifestyle'),
+        (DIY, 'Diy'),
+        (PARENTING, 'Parenting'),
+        (SOCIAL, 'Social'),
+        (STUDY, 'Study'),
+        (DAILY, 'Daily'),
+    ]
+    category = models.CharField(max_length=255,choices=CATEGORY_CHOICES,default=GENERAL)
     title = models.CharField(max_length=255)
     pub_date = models.DateTimeField()
     body = models.TextField()
@@ -10,7 +41,7 @@ class Blog(models.Model):
         return self.title
 
     def summary(self):
-        return self.body[:100]
+        return self.body[:100] + str(' ...')
 
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
